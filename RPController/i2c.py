1856afe29416a97732907b1a58777c8044759d7a
jraats <joost.de.man@gmail.com>
2016-01-12 22:33:08 +0100

    add arduino code, implemented i2c, set motors based on fake sensor data and preferences from api

---
+++ RPController/i2c.py
@@ -0,0 +1,48 @@
+import smbus
+from random import uniform
+import time
+
+class I2c:
+
+	def __init__(self):
+		self.bus = smbus.SMBus(1)
+	
+	def writeByte(self, address, value):
+		self.bus.write_byte(address, value)
+		time.sleep(0.001)
+		
+	def writeRegisterByte(self, address, register, value):
+		self.bus.write_byte_data(address, register, value)
+		time.sleep(0.001)
+
+	def readByte(self, address):
+		data = self.bus.read_byte(address)
+		time.sleep(0.001)
+		return data
+		
+	def readRegisterByte(self, address, register):
+		data = self.bus.read_byte_data(address, register)
+		time.sleep(0.001)
+		return data
+
+	def getTemperatureServo(self):
+		return self.readRegisterByte(0x04, 0x03)
+		
+	def setTemperatureServo(self, angle):
+		self.writeRegisterByte(0x04, 0x03, angle)
+	
+	def getLightServo(self):
+		return self.readRegisterByte(0x04, 0x05)	
+	
+	def setLightServo(self, speed):
+		self.writeRegisterByte(0x04, 0x05, speed)
+	
+	def readSensorTemperature(self):
+		#return self.readByte(0x01)
+		return 17 + uniform(0, 3)
+		
+	def readSensorLight(self):
+		#return self.readByte(0x01)
+		return 16 + uniform(0, 2)
+	
+		
